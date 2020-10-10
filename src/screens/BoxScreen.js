504b3ecd1529f046f2f9212287f25bf24e489436
Boris Mitioglov <boris_mitioglov@epam.com>
2020-10-09 22:11:46 -0700

    Layout experiments

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import { Text, View, StyleSheet} from "react-native";
+import { View, StyleSheet} from "react-native";
 
 
 const CustomBoxScreen = () => {
-    return <View>
-        <Text>Box Screen</Text>
+    return <View style={styles.customViewStyle}>
+        <View style={styles.viewStyle1}/>
+        <View style={styles.viewStyle2}/>
+        <View style={styles.viewStyle3}/>
     </View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    customViewStyle: {
+        borderWidth: 0,
+        // borderColor: 'purple',
+        height: 200,
+        // justifyContent: 'center'
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+        // justifyContent: 'flex-start'
+    },
+    viewStyle1: {
+        borderWidth: 3,
+        borderColor: 'purple',
+        width: 100
+        // flex: 5
+    },
+    viewStyle2: {
+        borderWidth: 3,
+        borderColor: 'red',
+        width: 100,
+        top: 195
+        // flex: 5,
+    },
+    viewStyle3: {
+        borderWidth: 3,
+        borderColor: 'green',
+        width: 100
+        // flex: 1
+    }
+});
 
 export default CustomBoxScreen;
